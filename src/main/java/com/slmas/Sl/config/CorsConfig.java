aab662785cd524c0caf4e8b7f7577a5e7567067a
Mbarca89 <mauriciobarca1989@gmail.com>
2024-07-31 09:21:24 -0300

    cors config

--- src/main/java/com/slmas/Sl/config/CorsConfig.java
+++ src/main/java/com/slmas/Sl/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("172.20.20.69")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
