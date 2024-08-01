1d393fa518ad8d90d8b44254719919bcad05dd53
Mbarca89 <mauriciobarca1989@gmail.com>
2024-08-01 11:00:36 -0300

    fix. image format

--- src/main/java/com/slmas/Sl/utils/ImageCompressor.java
+++ src/main/java/com/slmas/Sl/utils/ImageCompressor.java
@@ -7,12 +7,18 @@ import com.drew.metadata.exif.ExifIFD0Directory;
 import com.slmas.Sl.domain.Images;
 import org.imgscalr.Scalr;
 
+import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageOutputStream;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.Buffer;
+import java.util.Iterator;
 
 public class ImageCompressor {
 
@@ -27,8 +33,8 @@ public class ImageCompressor {
         BufferedImage resizedThumbnail = resizeImage(rotatedImage, 320, 184);
         // Convert resized image to WebP
 
-        byte [] fullImage = convertToWebP(resizedImage);
-        byte [] thumbnail = convertToWebP(resizedThumbnail);
+        byte [] fullImage = convertToJPG(resizedImage);
+        byte [] thumbnail = convertToJPG(resizedThumbnail);
         return new Images(fullImage, thumbnail);
     }
 
@@ -60,13 +66,33 @@ public class ImageCompressor {
         return Scalr.resize(image, Scalr.Method.QUALITY, Scalr.Mode.AUTOMATIC, targetWidth, targetHeight);
     }
 
-    private static byte[] convertToWebP(BufferedImage image) throws IOException {
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        try {
-            ImageIO.write(image, "jpg", outputStream);
-        } catch (Exception e) {
-            e.printStackTrace();
+    private static byte[] convertToJPG(BufferedImage image) throws IOException {
+        // Convert image to RGB if it has transparency (e.g., PNG with alpha channel)
+        if (image.getType() == BufferedImage.TYPE_INT_ARGB || image.getType() == BufferedImage.TYPE_4BYTE_ABGR) {
+            BufferedImage newImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_RGB);
+            Graphics2D g = newImage.createGraphics();
+            g.setComposite(AlphaComposite.Src);
+            g.setColor(Color.WHITE); // Set background color to white
+            g.fillRect(0, 0, image.getWidth(), image.getHeight());
+            g.drawImage(image, 0, 0, null);
+            g.dispose();
+            image = newImage;
         }
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName("jpg");
+        if (!writers.hasNext()) {
+            throw new IllegalStateException("ºNo JPG Image Writers found");
+        }
+        ImageWriter writer = writers.next();
+        ImageWriteParam param = writer.getDefaultWriteParam();
+        param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+        param.setCompressionQuality(0.85f); // Adjust the quality as needed
+        ImageOutputStream ios = ImageIO.createImageOutputStream(outputStream);
+        writer.setOutput(ios);
+        writer.write(null, new IIOImage(image, null, null), param);
+        ios.close();
+        writer.dispose();
         return outputStream.toByteArray();
     }
 }
